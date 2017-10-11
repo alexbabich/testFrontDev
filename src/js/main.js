c71113a36d617fcc1b0ec49c081b76cc266ee7fa
Alex Babych <ababich.dev@gmail.com>
2017-10-11 18:11:41 +0300

    begin add style for submenu and else element on header

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
     $('.selectpicker').selectpicker({
         style: 'dropdown-select'
     });
+
+    if ($('.dropdownUserInfo .dropdown-toggle').attr('title','My Account')) {
+        $('.dropdownUserInfo .dropdown-toggle .filter-option').addClass('fa fa-user');
+    }
+
+    $('.searchWrapper').focusin(function () {
+        $(this).addClass('onFocus');
+    });
+
+    $('.searchWrapper').focusout(function () {
+        $(this).removeClass('onFocus');
+    });
 });
